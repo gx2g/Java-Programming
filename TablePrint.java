f3d2e297bb22af7d7db47c99c4c66ea05bdb120f
Robert Resendez <grafx2g@gmail.com>
2018-09-02 18:13:57 -0700

    updated problem1

--- TablePrint.java
+++ TablePrint.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 class TablePrint {
     public static void main(String[] args) {
 
-
         for (int i = 1; i <= 10; i++) {
 
             for (int j = 1; j <= 10; j++) {
